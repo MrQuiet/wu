947be21b2d2f407eac4b20154fea00c6871c8d65
Dusty Wilson <dusty@linux.com>
2014-08-26 12:14:14 -0700

    More fields on the Update struct for your pleasure

--- example/ex1/ex1.go
+++ example/ex1/ex1.go
@@ -3,15 +3,17 @@ package main
 import (
 	"fmt"
 	"gopkg.in/wu.v0"
+	"strings"
 )
 
 func main() {
 	ses := wu.NewSession()
-	sea := ses.NewSearcher(true)
+	sea := ses.NewSearcher(false)
 	updates := sea.Query(wu.NewSearchQuery().IsInstalled(true).IsHidden(false).String())
 	fmt.Printf("Updates: %d\n", len(updates.Updates))
-	for i := 0; i < len(updates.Updates); i++ {
-		update := updates.Updates[i]
-		fmt.Printf("[%s] [%d] %s\nIsUninstallable[%t]\nUpdate URL[%s]\n\n", update.UpdateID, update.RevisionNumber, update.Title, update.IsUninstallable, update.SupportUrl)
+	for _, update := range updates.Updates {
+		securityBulletinIDs := "\t" + strings.Join(update.SecurityBulletinIDs, "\n\t")
+		supersededUpdateIDs := "\t" + strings.Join(update.SupersededUpdateIDs, "\n\t")
+		fmt.Printf("[%s]\n%s\nIsUninstallable[%t]\nRebootRequired[%t]\nUpdate URL[%s]\nSupersededUpdateIDs:\n%s\nSecurityBulletinIDs:\n%s\n\n", update.UpdateID, update.Title, update.IsUninstallable, update.RebootRequired, update.SupportUrl, supersededUpdateIDs, securityBulletinIDs)
 	}
 }
--- update.go
+++ update.go
@@ -11,16 +11,23 @@ type Updates struct {
 }
 
 type Update struct {
-	item            *ole.IDispatch
-	Title           string
-	Description     string
-	IsInstalled     bool
-	IsUninstallable bool
-	IsMandatory     bool
-	EulaAccepted    bool
-	RevisionNumber  int
-	UpdateID        string
-	SupportUrl      string
+	item                *ole.IDispatch
+	Title               string
+	Description         string
+	CanRequireSource    bool
+	IsBeta              bool
+	IsDownloaded        bool
+	IsInstalled         bool
+	IsUninstallable     bool
+	IsHidden            bool
+	IsMandatory         bool
+	RebootRequired      bool
+	EulaAccepted        bool
+	RevisionNumber      int
+	UpdateID            string
+	SupportUrl          string
+	SecurityBulletinIDs []string
+	SupersededUpdateIDs []string
 }
 
 func newUpdate(item *ole.IDispatch) *Update {
@@ -28,16 +35,41 @@ func newUpdate(item *ole.IDispatch) *Update {
 	up.item = item
 	up.Title = up.GetString("Title")
 	up.Description = up.GetString("Description")
+	up.CanRequireSource = up.GetBool("CanRequireSource")
+	up.IsBeta = up.GetBool("IsBeta")
+	up.IsDownloaded = up.GetBool("IsDownloaded")
 	up.IsInstalled = up.GetBool("IsInstalled")
 	up.IsUninstallable = up.GetBool("IsUninstallable")
+	up.IsHidden = up.GetBool("IsHidden")
 	up.IsMandatory = up.GetBool("IsMandatory")
+	up.RebootRequired = up.GetBool("RebootRequired")
 	up.EulaAccepted = up.GetBool("EulaAccepted")
-	identity := oleutil.MustGetProperty(item, "Identity").ToIDispatch()
-	revisionNumber := int(oleutil.MustGetProperty(identity, "RevisionNumber").Val)
-	up.RevisionNumber = revisionNumber
-	updateID := oleutil.MustGetProperty(identity, "UpdateID").ToString()
-	up.UpdateID = updateID
+	{
+		identity := oleutil.MustGetProperty(item, "Identity").ToIDispatch()
+		revisionNumber := int(oleutil.MustGetProperty(identity, "RevisionNumber").Val)
+		up.RevisionNumber = revisionNumber
+		updateID := oleutil.MustGetProperty(identity, "UpdateID").ToString()
+		up.UpdateID = updateID
+	}
 	up.SupportUrl = up.GetString("SupportUrl")
+	{
+		securityBulletinIDs := oleutil.MustGetProperty(item, "SecurityBulletinIDs").ToIDispatch()
+		count := int(oleutil.MustGetProperty(securityBulletinIDs, "Count").Val)
+		up.SecurityBulletinIDs = make([]string, count)
+		for i := 0; i < count; i++ {
+			securityBulletinID := oleutil.MustGetProperty(securityBulletinIDs, "Item", i).ToString()
+			up.SecurityBulletinIDs[i] = securityBulletinID
+		}
+	}
+	{
+		supersededUpdateIDs := oleutil.MustGetProperty(item, "SupersededUpdateIDs").ToIDispatch()
+		count := int(oleutil.MustGetProperty(supersededUpdateIDs, "Count").Val)
+		up.SupersededUpdateIDs = make([]string, count)
+		for i := 0; i < count; i++ {
+			supersededUpdateID := oleutil.MustGetProperty(supersededUpdateIDs, "Item", i).ToString()
+			up.SupersededUpdateIDs[i] = supersededUpdateID
+		}
+	}
 	return up
 }
 
