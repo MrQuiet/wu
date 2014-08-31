eb24369ae694bb85bfb18c9788fa294b91d02885
Dusty Wilson <dusty@linux.com>
2014-08-30 22:42:21 -0700

    Started example ex2

---
+++ example/ex2/ex2.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"gopkg.in/wu.v0"
+)
+
+func main() {
+	ses := wu.NewSession()
+	sea := ses.NewSearcher(true)
+	updates := sea.Query(wu.NewSearchQuery().IsInstalled(false).IsHidden(false).String())
+	fmt.Printf("Updates: %d\n", len(updates.Updates))
+	for _, update := range updates.Updates {
+		fmt.Printf("[%s] %s\n", update.Type, update.Title)
+	}
+}
